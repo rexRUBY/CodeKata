bc3bb5d0b5f4925e5ea650d5c7f21e32ec25873f
Yeseo Park <87340601+rexRUBY@users.noreply.github.com>
2024-10-19 17:39:55 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.51 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -0,0 +1,12 @@
+def solution(n):
+    answer = 0
+    dp = [0] * (n+1)
+    
+    dp[1] = 1
+    if(n>=2):
+        dp[2] = 2
+    
+    for i in range(3, n+1):
+        dp[i] = dp[i-2] + dp[i-1]
+    
+    return dp[n] % 1234567
